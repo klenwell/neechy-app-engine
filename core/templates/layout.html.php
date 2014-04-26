4a7ffadf3719efe64ac4340d63a23f003b53751d
klenwell <klenwell@gmail.com>
2014-04-26 16:40:19 -0700

    adds layout file with basic templating

---
+++ core/templates/layout.html.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  {{ head }}
+  <body>
+    <div class="container">
+      {{ top }}
+
+      {{ middle }}
+    </div>
+
+    {{ bottom }}
+  </body>
+</html>
--- public/index.php
+++ public/index.php
@@ -1,19 +1,31 @@
 <?php
-
-    $html = <<<HTML5
-<!doctype html>
-<html lang="en">
+    $partials = array();
+    $partials['head'] = <<<HTML5
 <head>
   <title>Neechy</title>
 </head>
+HTML5;
 
-<body>
-  <h1>Neechy</h1>
+    $partials['top'] = '<h1>Neechy</h1>';
+
+    $partials['middle'] = <<<HTML5
   <p>For more information, visit
     <a href="https://github.com/klenwell/neechy">the Neechy Github site</a>.
   </p>
-</body>
-</html>
 HTML5;
 
-    print $html;
+    $partials['bottom'] = <<<HTML5
+  <footer>
+    some rights reserved
+  </footer>
+HTML5;
+
+
+    $layout_file = '../core/templates/layout.html.php';
+    $output = file_get_contents($layout_file);
+
+    foreach ($partials as $partial => $content) {
+        $output = str_replace(sprintf('{{ %s }}', $partial), $content, $output);
+    }
+
+    print $output;
