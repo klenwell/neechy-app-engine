5b3113d91142828e3cbaa916de49e025f957b959
klenwell <klenwell@gmail.com>
2014-04-26 11:18:27 -0700

    adds index.php file

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+
+    $html = <<<HTML5
+<!doctype html>
+<html lang="en">
+<head>
+  <title>Neechy</title>
+</head>
+
+<body>
+  <h1>Neechy</h1>
+  <p>For more information, visit
+    <a href="https://github.com/klenwell/neechy">the Neechy Github site</a>.
+  </p>
+</body>
+</html>
+HTML5;
+
+    print $html;
