bbbc0d0ae3c21378b3d9bc631f5e052332dc8568
klenwell <klenwell@gmail.com>
2014-04-27 21:43:18 -0700

    add user and core conf files

---
+++ core/config/core.conf.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Neechy Main Configuration File
+ *
+ */
+
+$neechy_core_conf = array(
+    # Version
+    'version'           => '0.1',
+
+    # Database Settings
+    'mysql_database'    => 'NULL',
+    'mysql_host'        => 'NULL',
+    'mysql_password'    => 'NULL',
+    'mysql_user'        => 'NULL',
+
+    # Wiki Settings
+    'title'             => 'Neechy',
+    'keywords'          => 'wikka, wiki, niche, nietzsche',
+    'description'       => 'a lightweight wiki platform adapted from WikkaWiki',
+    'theme'             => 'bootstrap'
+);
