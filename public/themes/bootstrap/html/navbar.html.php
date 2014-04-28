05bf4b5b63558d3d6a9f4e5ae660d57831cc6e9c
klenwell <klenwell@gmail.com>
2014-04-27 17:02:44 -0700

    adds login/signup button to navbar

--- public/themes/bootstrap/html/navbar.html.php
+++ public/themes/bootstrap/html/navbar.html.php
@@ -8,13 +8,25 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a href="#" class="navbar-brand">Neechy</a>
+          <a href="/" class="navbar-brand">Neechy</a>
         </div>
+
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
+            <!-- TODO: dynamically build menu -->
             <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a href="#">Categories</a></li>
+            <li><a href="#">PageIndex</a></li>
+            <li><a href="#">RecentChanges</a></li>
+            <li><a href="#">RecentComments</a></li>
+          </ul>
+
+          <ul class="nav navbar-nav navbar-right">
+            <li>
+                <div class="a-requires-parent">
+                  <a href="#" class="btn btn-primary navbar-btn">Login / SignUp</a>
+                </div>
+            </li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
