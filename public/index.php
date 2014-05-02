7a637be9f998efb662806e1c7f2ee667951497bb
klenwell <klenwell@gmail.com>
2014-05-01 21:59:40 -0700

    adds NeechyRequest class and tests

--- public/index.php
+++ public/index.php
@@ -7,17 +7,27 @@
  *
  */
 require_once('../core/libs/config.php');
+require_once('../core/libs/request.php');
 require_once('../core/libs/templater.php');
 
 
-$content = <<<HTML5
+# Init config here for usage elsewhere
+NeechyConfig::init();
+
+$request = new NeechyRequest();
+
+if ( $request->is('edit') ) {
+    require_once('../core/handlers/edit/handler.php');
+    $handler = new EditHandler($request);
+    $content = $handler->handle();
+}
+else {
+    $content = <<<HTML5
   <p>For more information, visit
     <a href="https://github.com/klenwell/neechy">the Neechy Github site</a>.
   </p>
 HTML5;
-
-# Init config here for usage elsewhere
-NeechyConfig::init();
+}
 
 $templater = new NeechyTemplater();
 $templater->set('content', $content);
