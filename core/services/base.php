b3e60339a440ea1ddfb97139eae9eec6f6751275
klenwell <klenwell@gmail.com>
2014-05-05 11:02:13 -0700

    stubs out NeechyService and NeechyWebService classes; moves main script logic over to new service class

---
+++ core/services/base.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * core/services/base.php
+ *
+ * NeechyService base class.
+ *
+ */
+require_once('../core/neechy/config.php');
+
+
+class NeechyService {
+    #
+    # Properties
+    #
+
+    #
+    # Constructor
+    #
+    public function __construct() {
+        NeechyConfig::init();
+    }
+
+    #
+    # Public Methods
+    #
+    public function serve() {
+    }
+
+    public function serve_error() {
+    }
+}
+
