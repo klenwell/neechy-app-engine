1865f49402f2aa09501bbbcecddbafb39ecc55c7
klenwell <klenwell@gmail.com>
2014-05-08 07:07:20 -0700

    adds user model and tests

---
+++ core/models/user.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * core/models/user.php
+ *
+ * Neechy User model class.
+ *
+ */
+require_once('../core/models/base.php');
+
+
+class User extends NeechyModel {
+
+    protected static $schema = <<<MYSQL
+CREATE TABLE users (
+	id int(11) NOT NULL auto_increment,
+    name varchar(255) NOT NULL default '',
+	email varchar(255) NOT NULL default '',
+	password varchar(255) NOT NULL default '',
+	status varchar(16) NOT NULL default '',
+	challenge varchar(8) default '',
+
+	theme varchar(64) default '',
+	show_comments enum('Y','N') NOT NULL default 'N',
+
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated_at DATETIME default NULL,
+
+	PRIMARY KEY (id),
+    KEY idx_name (name),
+	KEY idx_created_at (created_at)
+) CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE={{ engine }}
+MYSQL;
+
+    /*
+     * Constructor
+     */
+
+    /*
+     * Static Methods
+     */
+    public static function find_by_name($name) {
+        $sql = "SELECT * FROM users WHERE name = ? ORDER BY updated_at DESC LIMIT 1";
+
+        $pdo = NeechyDatabase::connect_to_db();
+        $query = $pdo->prepare($sql);
+        $query->execute(array($name));
+        $row = $query->fetch(PDO::FETCH_ASSOC);
+
+        if ( $row ) {
+            $user = new User($row);
+        }
+        else {
+            $user = new User(array('name' => $name));
+        }
+
+        return $user;
+    }
+
+    /*
+     * Instance Methods
+     */
+    public function save() {
+        $sql_f = 'INSERT INTO users (%s, updated_at) VALUES (%s, NOW())';
+
+        # Use database values
+        $this->un_set('id');
+        $this->un_set('updated_at');
+
+        $sql = sprintf($sql_f,
+            implode(', ', array_keys($this->fields)),
+            implode(', ', array_fill(0, count($this->fields), '?'))
+        );
+
+        $query = $this->pdo->prepare($sql);
+        $query->execute(array_values($this->fields));
+        return $query;
+    }
+
+    public function is_new() {
+        return is_null($this->field('id'));
+    }
+
+	public function url($handler=NULL, $action=NULL, $params=array()) {
+		return NeechyPath::url($this->field('name'), $handler, $action, $params);
+	}
+}
---
+++ test/models/UserTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * test/models/UserTest.php
+ *
+ * Usage (run from root dir):
+ * > phpunit --bootstrap test/bootstrap.php models/UserTest
+ *
+ */
+require_once('../core/models/user.php');
+require_once('../test/helper.php');
+require_once('../test/fixtures/user.php');
+
+
+class PageModelTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * Test Fixtures
+     */
+    public function setUp() {
+        NeechyTestHelper::setUp();
+        UserFixture::init();
+    }
+
+    public function tearDown() {
+        NeechyTestHelper::tearDown();
+    }
+
+    /**
+     * Tests
+     */
+    public function testFindByName() {
+        $user = User::find_by_name('NeechyUser');
+        $this->assertEquals('NeechyUser', $user->field('name'));
+    }
+
+    public function testSave() {
+        $user = new User(array('name' => 'FreddyNeechy'));
+        $query = $user->save();
+        $this->assertEquals(1, $query->rowCount());
+    }
+
+    public function testInstantiates() {
+        $user = new User();
+        $this->assertInstanceOf('User', $user);
+        $this->assertEquals('users', $user->table);
+    }
+}
