0d190fc6aefd81d20d5698876597ceabfd23cad5
klenwell <klenwell@gmail.com>
2014-05-12 09:27:03 -0700

    update boilerplate for new user page

--- core/handlers/auth/html/new-page.md.php
+++ core/handlers/auth/html/new-page.md.php
@@ -7,8 +7,12 @@
 $t = $this;   # templater object
 
 ?>
-# Welcome to your new page
+# Welcome to the <?php echo NeechyConfig::get('title'); ?> Wiki
 
-## This will be changed in future.
+## This is your wiki page!
 
-Return [home](/?page=HomePage)
+To deter spammers, this page will not be visible to search engines. When you registered, an email was sent to you. As soon as you click the link in that email, you will be able to edit this page.
+
+It will be the only page you edit until an administrator upgrades you. In the meantime, feel free look around, learn more about [markdown](?) (the formatting syntax this wiki uses), and rewrite this page to your heart's content.
+
+Welcome aboard!
