725a38d792964ba1319fe13484d9c0399612d54b
klenwell <klenwell@gmail.com>
2014-05-18 07:31:53 -0700

    adds console run interface

---
+++ console/run.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * run.php
+ *
+ * This is the main Neechy console script. It provides a command line interface
+ * for running handlers and tasks.
+ *
+ */
+#require_once('../core/services/console.php');
+
+#$console_service = new NeechyConsoleService();
+#$console_service->serve();
+
+print_r($_SERVER);
