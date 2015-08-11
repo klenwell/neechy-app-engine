892c7ec376f625584a304c0aadd04b4c70db79b6
klenwell <klenwell@gmail.com>
2015-08-11 07:41:19 -0700

    Stubs out preferences handler.

---
+++ core/handlers/preferences/handler.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * core/handlers/preferences/handler.php
+ *
+ * PreferencesHandler class.
+ *
+ */
+require_once('../core/handlers/base.php');
+require_once('../core/neechy/path.php');
+require_once('../core/neechy/templater.php');
+
+
+class PreferencesHandler extends NeechyHandler {
+
+    #
+    # Public Methods
+    #
+    public function handle() {
+        # Change password request
+        if ( $this->request->action_is('change_password') ) {
+            # TODO: process request
+            $content = $this->render_view('content');
+        }
+
+        # Default: display
+        else {
+            $content = $this->render_view('content');
+        }
+
+        return $content;
+    }
+
+    #
+    # Private
+    #
+}
---
+++ core/handlers/preferences/html/content.html.php
@@ -0,0 +1,20 @@
+<?php
+#
+# Neechy Preferences View
+#
+
+$t = $this;   # templater object
+#$t->append_to_head($t->css_link($t->css_href('login.css')));
+
+# General vars
+
+
+#
+# Helper function
+#
+
+
+?>
+    <div class="preferences">
+      <h2>TO DO: Preferences</h2>
+    </div>
