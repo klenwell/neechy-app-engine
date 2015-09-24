15d3149841e03b2b59acedd45754cc3d9ead6e49
klenwell <klenwell@gmail.com>
2015-09-23 19:03:54 -0700

    Stubs out history handler.

---
+++ core/handlers/history/handler.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * core/handlers/page/handler.php
+ *
+ * PageHandler class.
+ *
+ */
+require_once('../core/handlers/base.php');
+require_once('../core/neechy/templater.php');
+require_once('../core/neechy/response.php');
+
+
+class HistoryHandler extends NeechyHandler {
+    #
+    # Public Methods
+    #
+    public function handle() {
+        return 'TODO: render history';
+    }
+
+    #
+    # Private Methods
+    #
+}
