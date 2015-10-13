2d60e41ed661b66645f7c4d03c6aedd6a645239f
klenwell <klenwell@gmail.com>
2015-10-12 21:55:44 -0700

    Adds template core pages.

---
+++ app/templates/core_pages/NeechySystem.md.php
@@ -0,0 +1,9 @@
+<?php
+#
+# Default NeechySystem Page
+# This gets saved when install script is run
+#
+?>
+## NeechySystem
+
+NeechySystem is the system user created upon system install and responsible for creating default pages, like this one.
