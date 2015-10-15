2797463f0d53923e079f9c54218e4b083d335150
klenwell <klenwell@gmail.com>
2015-10-14 20:53:00 -0700

    Changes core home page template and removes unimplemented navbar links.

--- app/templates/core_pages/home.md.php
+++ app/templates/core_pages/home.md.php
@@ -4,18 +4,14 @@
 # This gets saved when install script is run
 #
 ?>
-## Congratulations on your new Neechy Wiki
+## Welcome to the Neechy App Engine Demo.
 
-You're new wiki, <?php echo NeechyConfig::get('title'); ?>, is ready!
+This is a demo of the Neechy PHP wiki project. It is designed to run on the [Google PHP App Engine runtime environment](https://cloud.google.com/appengine/docs/php/) and the code is available on [Github](https://github.com/klenwell/neechy-app-engine).
 
 ### Login and Start Editing
 
-Once you log in, you can start editing wiki pages. Neechy Wiki supports an extended markdown syntax. For more information, see the [NeechyFormatting page](/?page=NeechyFormatting).
-
-### Administration
-
-If you are an administrator, you'll have additional tools at your disposal to help you manage your wiki once you log in.
+Login by clicking the login button in the navbar and using your Google account. Once you log in, you can start editing wiki pages. Neechy Wiki supports an extended markdown syntax. For more information, see the [NeechyFormatting page](/?page=NeechyFormatting).
 
 ### Developers
 
-Neechy is designed to allow you, as a developer, to hack on your own wiki. Extend existing features and add your own. For more information, visit the Neechy github site.
+Neechy is designed to allow you, as a developer, to hack on your own wiki. Extend existing features and add your own. For more information, visit the [Neechy Github site](https://github.com/klenwell/neechy).
