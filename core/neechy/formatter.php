037c9dda36495d547a3aa1307110fec9297f17e9
klenwell <klenwell@gmail.com>
2015-11-23 08:02:21 -0800

    Adds preprocessor for wikka-style links along with tests. This will not work for case where links inside code blocks should be bypassed.

--- core/neechy/formatter.php
+++ core/neechy/formatter.php
@@ -30,7 +30,7 @@ class NeechyFormatter  {
     # Public Methods
     #
     public function wml_to_html($wml) {
-        $wml = $this->wikka_links_to_markup_links($wml);
+        $wml = $this->wikka_links_to_markdown_links($wml);
         $html = $this->markdown->text($wml);
         return $html;
     }
@@ -38,8 +38,69 @@ class NeechyFormatter  {
     #
     # Protected Methods
     #
-    protected function wikka_links_to_markup_links($wml) {
-        # TODO
+    protected function wikka_links_to_markdown_links($wml) {
+        $wml = $this->replace_double_bracket_links($wml);
+        $wml = $this->replace_title_case_links($wml);
         return $wml;
+    }
+
+    protected function replace_title_case_links($wml) {
+        # Source: http://stackoverflow.com/a/815849/1093087
+        $regex_parts = array(
+            '(?<=\s|^)',    # either a word boundary or position 0
+                            # avoid cases like [Already in markup link](TitleCase)
+                            # http://stackoverflow.com/a/14074352/1093087
+            '[A-Z]',        # capital letter
+            '[a-zA-Z]*',    # all alphabetic chars
+            '(?:',          # look-behind?
+                '[a-z][a-zA-Z]*[A-Z]',
+                '|',
+                '[A-Z][a-zA-Z]*[a-z]',
+            ')',
+            '[a-zA-Z]*'     # all alphabetic chars
+        );
+        $regex = sprintf('/%s/msu', join('', $regex_parts));
+        $wml = preg_replace_callback($regex,
+                                     array($this, 'title_case_to_markdown_link'),
+                                     $wml);
+        return $wml;
+    }
+
+    protected function replace_double_bracket_links($wml) {
+        $regex_parts = array("\[\[\s*(.*?)\s*\]\]");
+        $regex = sprintf('/%s/msu', join('', $regex_parts));
+        $wml = preg_replace_callback($regex,
+                                     array($this, 'double_brackets_to_markdown_link'),
+                                     $wml);
+        return $wml;
+    }
+
+    protected function title_case_to_markdown_link($matches) {
+        $markdown_format = '[%s](/page/%s)';
+        $camel_case = $matches[0];
+        return sprintf($markdown_format, $camel_case, $camel_case);
+    }
+
+    protected function double_brackets_to_markdown_link($matches) {
+        $markdown_format = '[%s](%s)';
+        $meat = $matches[1];
+
+        if ( strpos($meat, '|') !== false ) {
+            list($href, $label) = explode('|', $meat, 2);
+        }
+        else {
+            list($href, $label) = explode(' ', $meat, 2);
+        }
+
+        $protocol_regex = sprintf('~(%s)://~su', 'http|https|ftp|news|irc|gopher');
+        $with_protocol = preg_match($protocol_regex, $href);
+        if ( $with_protocol ) {
+            $href = trim($href);
+        }
+        else {
+            $href = sprintf('/page/%s', trim($href));
+        }
+
+        return sprintf($markdown_format, trim($label), $href);
     }
 }
--- test/neechy/FormatterTest.php
+++ test/neechy/FormatterTest.php
@@ -24,6 +24,39 @@ class FormatterTest extends PHPUnit_Framework_TestCase {
      * Tests
      */
     public function testWikkaLinkTranslation() {
+        $test_cases = array(
+            # [$wml, $expected]
+            array('[CamelLink](/page/CamelLink)',
+                  '<p><a href="/page/CamelLink">CamelLink</a></p>'),
+            array('CamelLink',
+                  '<p><a href="/page/CamelLink">CamelLink</a></p>'),
+            array('word CamelLink word',
+                  '<p>word <a href="/page/CamelLink">CamelLink</a> word</p>'),
+            array('[[CamelLink Camel Link]]',
+                  '<p><a href="/page/CamelLink">Camel Link</a></p>'),
+            array('x [[CamelLink Camel Link]] x',
+                  '<p>x <a href="/page/CamelLink">Camel Link</a> x</p>'),
+            array('[[CamelLink | Camel Link]]',
+                  '<p><a href="/page/CamelLink">Camel Link</a></p>'),
+            array('[[http://github.com/ Github]]',
+                  '<p><a href="http://github.com/">Github</a></p>'),
+            array('x [[http://github.com/ Github]] x',
+                  '<p>x <a href="http://github.com/">Github</a> x</p>'),
+            array('[[http://github.com/ | Github]]',
+                  '<p><a href="http://github.com/">Github</a></p>'),
+
+            # Leave links in code blocks alone
+            array('    NeechyFormatting',
+                  '<pre><code>NeechyFormatting</code></pre>')
+        );
+
+        $formatter = new NeechyFormatter();
+
+        foreach ( $test_cases as $case ) {
+            list($wml, $expected) = $case;
+            $html = $formatter->wml_to_html($wml);
+            $this->assertEquals($expected, $html, sprintf('Failed for case: %s', $wml));
+        }
     }
 
     public function testInstantiates() {
